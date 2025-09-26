2c97af6631a7e3c9ba19607f3536aa2a0b78f9bf
benmacfre <benjamin.freyter@gmail.com>
2025-09-26 10:59:02 +0200

    Update version to 0.2.6 and add custom error classes for improved error handling

---
+++ cnquant_dependencies/custom_errors.py
@@ -0,0 +1,24 @@
+class Missing_idat(Exception):
+    """Reports errors related to missing idat files"""
+    def __init__(self, message="Missing idat files"):
+        self.message = "Missing idat files"
+        super().__init__(self.message)
+
+class Unsupported_array_type(Exception):
+    """Reports errors related to missing idat files"""
+    def __init__(self, array_type: str, message="The array type is not supported"):
+        self.message = message
+        self.array_type = array_type
+        super().__init__(self.message)
+
+class Failure_to_load_reference_data(Exception):
+    """Reports errors related to loading reference data"""
+    def __init__(self, error: str):
+        self.message = f"Failed to load reference data.\nError: {error}"
+        super().__init__(self.message)
+
+class Failure_to_convert_sentrix_id(Exception):
+    """Reports errors related to converting idat to 450k format"""
+    def __init__(self, sentrix_id: str = "missing sentrix id",  error: str = "missing error"):
+        self.message = f"Failed to convert idat pair id: {sentrix_id}.\nError: {error}"
+        super().__init__(self.message)
