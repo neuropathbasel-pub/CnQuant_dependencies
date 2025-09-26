fcf312c1a7bc7270830a2c5c1db0cf93214a1006
benmacfre <benjamin.freyter@gmail.com>
2025-09-26 15:39:54 +0200

    Refactor CommonArrayType enum values and update related tests for consistency

--- cnquant_dependencies/CommonArrayType.py
+++ cnquant_dependencies/CommonArrayType.py
@@ -5,19 +5,19 @@ from typing import Sequence, cast
 @unique
 class CommonArrayType(Enum):
     """Provides constants lists for the final downsized array type."""
-    EPIC_v2_to_HM450K = "EPIC_v2_to_HM450K"
-    EPIC_v2_to_MSA48 = "EPIC_v2_to_MSA48"
+    EPIC_v2_EPIC_v1_to_HM450K = "EPIC_v2_EPIC_v1_to_HM450K"
+    EPIC_v2_EPIC_v1_HM450_to_MSA48 = "EPIC_v2_EPIC_v1_HM450_to_MSA48"
 
     @classmethod
     def get_array_types(cls, convert_from_to: "CommonArrayType") -> list[ArrayType]:
         """Returns the list of ArrayType instances for the given CommonArrayType enum value."""
-        if convert_from_to == cls.EPIC_v2_to_HM450K:
+        if convert_from_to == cls.EPIC_v2_EPIC_v1_to_HM450K:
             return [
                 ArrayType.ILLUMINA_450K,
                 ArrayType.ILLUMINA_EPIC,
                 ArrayType.ILLUMINA_EPIC_V2,
             ]
-        elif convert_from_to == cls.EPIC_v2_to_MSA48:
+        elif convert_from_to == cls.EPIC_v2_EPIC_v1_HM450_to_MSA48:
             return [
                 ArrayType.ILLUMINA_MSA48,
                 ArrayType.ILLUMINA_450K,
--- tests/test_CommonArrayType.py
+++ tests/test_CommonArrayType.py
@@ -3,14 +3,14 @@ from cnquant_dependencies.ArrayType import ArrayType
 
 def test_CommonArrayType():
     all_members_set = set(CommonArrayType.members_list())
-    correct_all_members_set = {CommonArrayType.EPIC_v2_to_HM450K, CommonArrayType.EPIC_v2_to_MSA48}
+    correct_all_members_set = {CommonArrayType.EPIC_v2_EPIC_v1_to_HM450K, CommonArrayType.EPIC_v2_EPIC_v1_HM450_to_MSA48}
     assert all_members_set == correct_all_members_set, f"members_list() returned {all_members_set}, expected {correct_all_members_set}"
-    assert all(True for element in CommonArrayType.get_array_types(CommonArrayType.EPIC_v2_to_HM450K) if element in all_members_set), f"EPIC_v2_to_HM450K contains invalid ArrayType members: {[element for element in CommonArrayType.get_array_types(CommonArrayType.EPIC_v2_to_HM450K) if element not in all_members_set]}"
-    assert all(True for element in CommonArrayType.get_array_types(CommonArrayType.EPIC_v2_to_MSA48) if element in all_members_set), f"EPIC_v2_to_MSA48 contains invalid ArrayType members: {[element for element in CommonArrayType.get_array_types(CommonArrayType.EPIC_v2_to_MSA48) if element not in all_members_set]}"
-    value_to_key_mapping = CommonArrayType.value_to_key_mapping(common_array_types=[CommonArrayType.EPIC_v2_to_HM450K, CommonArrayType.EPIC_v2_to_MSA48])
+    assert all(True for element in CommonArrayType.get_array_types(CommonArrayType.EPIC_v2_EPIC_v1_to_HM450K) if element in all_members_set), f"EPIC_v2_EPIC_v1_to_HM450K contains invalid ArrayType members: {[element for element in CommonArrayType.get_array_types(CommonArrayType.EPIC_v2_EPIC_v1_to_HM450K) if element not in all_members_set]}"
+    assert all(True for element in CommonArrayType.get_array_types(CommonArrayType.EPIC_v2_EPIC_v1_HM450_to_MSA48) if element in all_members_set), f"EPIC_v2_EPIC_v1_HM450_to_MSA48 contains invalid ArrayType members: {[element for element in CommonArrayType.get_array_types(CommonArrayType.EPIC_v2_EPIC_v1_HM450_to_MSA48) if element not in all_members_set]}"
+    value_to_key_mapping = CommonArrayType.value_to_key_mapping(common_array_types=[CommonArrayType.EPIC_v2_EPIC_v1_to_HM450K, CommonArrayType.EPIC_v2_EPIC_v1_HM450_to_MSA48])
     shall_be_mapping = {
-        "EPIC_v2_to_HM450K": "EPIC_v2_to_HM450K",
-        "EPIC_v2_to_MSA48": "EPIC_v2_to_MSA48"
+        "EPIC_v2_EPIC_v1_to_HM450K": "EPIC_v2_EPIC_v1_to_HM450K",
+        "EPIC_v2_EPIC_v1_HM450_to_MSA48": "EPIC_v2_EPIC_v1_HM450_to_MSA48"
     }
     assert sorted([value_to_key_mapping.items()]) == sorted([shall_be_mapping.items()]), f"value_to_key_mapping returned {value_to_key_mapping}, expected {shall_be_mapping}"
 
