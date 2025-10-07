f99c2264dc3b938501a436ee852e98c86b4f9cc7
benmacfre <benjamin.freyter@gmail.com>
2025-10-07 17:05:24 +0200

    Update version to 0.4.6 and enhance PreprocessingMethods class with members_list and valid_preprocessing_methods methods

--- cnquant_dependencies/PreprocessingMethods.py
+++ cnquant_dependencies/PreprocessingMethods.py
@@ -1,5 +1,5 @@
 from enum import Enum, unique
-
+from typing import Sequence, cast
 @unique
 class PreprocessingMethods(Enum):
     "Enum class holding all acceptable preprocessing methods for CnQuant"
@@ -11,9 +11,21 @@ class PreprocessingMethods(Enum):
     @classmethod
     def list(cls):
         return list(cls)
+    
+    @classmethod
+    def members_list(cls) -> Sequence["PreprocessingMethods"]:
+        """Returns a sequence of the string values of all CommonArrayType enum members."""
+        return cast(Sequence[PreprocessingMethods], [member for member in cls])
+    
+    @classmethod
+    def valid_preprocessing_methods(cls) -> Sequence[str]:
+        """Returns a list of array types currently supported by CnQuant."""
+        return cast(Sequence[str], [member.value for member in cls])
 
 def get_enum_from_string(value: str) -> PreprocessingMethods | None:
     for name, member in PreprocessingMethods.__members__.items():
         if member.value == value.lower():
             return member
     return None
+
+    
