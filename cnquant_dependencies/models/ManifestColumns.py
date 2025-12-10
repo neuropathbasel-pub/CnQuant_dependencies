ca72540c263722a304dbb2d947a07586d7bdb1f1
benmacfre <benjamin.freyter@gmail.com>
2025-12-10 16:52:36 +0100

    Add clean_gene_names function and valid_manifest_chromosomes list; bump version to 0.13.2

--- cnquant_dependencies/models/ManifestColumns.py
+++ cnquant_dependencies/models/ManifestColumns.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 PROBES_COLUMNS: list[str] = [
     "IlmnID",
     "Name",
@@ -22,3 +24,37 @@ CONTROL_COLUMNS: tuple[str, str, str, str] = (
     "Extended_Type",
 )
 NONE: int = -1
+
+def clean_gene_names(value):
+    if pd.isna(value):
+        return value  # Keep NaN as is
+    # Split by ";", remove duplicates with set, join back
+    return ";".join(set(value.split(";")))
+
+
+valid_manifest_chromosomes: list[str] = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "X",
+    "Y",
+]
